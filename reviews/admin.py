cf0e6c78ea0ac4eeccf391e330b79636062414dd
Valeron <valerashankorenko@yandex.by>
2024-11-17 16:19:40 +0300

    Правки CSS

--- reviews/admin.py
+++ reviews/admin.py
@@ -5,12 +5,18 @@ from .models import Review
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
-        'author', 'text', 'created_at_format', 'is_published')
-    list_display_links = ('created_at_format', 'author',)
-    search_fields = ('created_at_format', 'author',)
+        'get_author_name', 'text', 'created_at_format', 'is_published'
+    )
+    list_display_links = ('created_at_format', 'get_author_name',)
+    search_fields = ('text', 'author__email',)
     empty_value_display = '-пусто-'
+
+    def get_author_name(self, obj):
+        return f'{obj.author.first_name} {obj.author.last_name}'
+    get_author_name.short_description = 'Автор отзыва'
 
     def created_at_format(self, obj):
         return obj.created_at.strftime('%d-%m-%Y %H:%M:%S')
 
     created_at_format.short_description = 'Дата и время отзыва'
+
