d1178648e0164d370f86ca52cb7c530c7a6f4da2
Valeron <valerashankorenko@yandex.by>
2025-01-30 22:56:47 +0300

    Pytest тесты 1

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView, ListView, DetailView
 from http import HTTPStatus
--- pytest_tests/test_routes.py
+++ pytest_tests/test_routes.py
@@ -3,12 +3,58 @@ from http import HTTPStatus
 import pytest
 from django.urls import reverse
 
+pytestmark = pytest.mark.django_db
+
 
 @pytest.mark.parametrize(
     'name',
-    ('pages:index',)
+    (
+            # Pages app
+            'pages:index',
+            'pages:price',
+            'pages:foto',
+            'pages:contact',
+            'pages:info',
+
+            # Users app
+            'users:registration',
+            'login',
+
+            # Reviews app
+            'reviews:review',
+
+    ),
+    ids=[
+        'Главная страница',
+        'Прайс лист',
+        'Фото работ',
+        'Контакты',
+        'Советы',
+        'Регистрация пользователя',
+        'Страница входа',
+        'Список отзывов',
+    ]
 )
 def test_pages_availability_for_anonymous_user(client, name):
+    """
+    Тестирует доступность публичных страниц для анонимных пользователей.
+
+    Проверяет, что указанные URL-адреса:
+    - Возвращают статус 200 OK
+    - Не требуют аутентификации
+    - Доступны без дополнительных параметров
+
+    Параметры теста:
+    - name: Имя URL-шаблона в формате 'app:url_name'
+    """
     url = reverse(name)
-    response = client.get(url)
-    assert response.status_code == HTTPStatus.OK
+
+    try:
+        response = client.get(url)
+    except Exception as e:
+        pytest.fail(f'Ошибка при обращении к странице {name}: {str(e)}')
+
+    assert response.status_code == HTTPStatus.OK, (
+        f'Страница {name} должна быть доступна анонимным пользователям. '
+        f'Получен статус: {response.status_code}'
+    )
